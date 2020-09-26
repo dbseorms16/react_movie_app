070095152ce47feec79b7097f7a7e14962a0ad26
Jun seok <dbseorms16@naver.com>
2020-09-26 13:04:07 +0900

    Updates

---
+++ precache-manifest.3920aaaf025fc74a5285877d694ca226.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da9eac53663b21e85c36c516f545c2cd",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "5f004fa2537dc45d0be0",
+    "url": "/react_movie_app/static/css/main.4fe61321.chunk.css"
+  },
+  {
+    "revision": "b9cb88537a0f4167021b",
+    "url": "/react_movie_app/static/js/2.1c14cd22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_movie_app/static/js/2.1c14cd22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f004fa2537dc45d0be0",
+    "url": "/react_movie_app/static/js/main.dc19a10d.chunk.js"
+  },
+  {
+    "revision": "00ec9c2259bc5a9a77bf",
+    "url": "/react_movie_app/static/js/runtime-main.7e6802fe.js"
+  }
+]);
